4308523bb5c845b83203f54d100bb27f59f5c99c
Dustin Lim <dustinlim26@gmail.com>
2025-08-31 11:06:49 +0800

    finished user_config_view

--- include/c_space_manipulator/views/user_config_view.hpp
+++ include/c_space_manipulator/views/user_config_view.hpp
@@ -1,4 +1,9 @@
 #include <QWidget>
+#include <QPushButton>
+#include <QScrollArea>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QMessageBox>
 #include "c_space_manipulator/views/link_config_view.hpp"
 
 namespace cspace_manipulator
@@ -9,6 +14,16 @@ class UserConfigView : public QWidget
     public:
     UserConfigView(QWidget *parent = nullptr);
     ~UserConfigView();
+
+    private:
+    std::vector<LinkConfigView*> link_config_list_;
+    QVBoxLayout* links_layout_
+    int max_link_num_ = 10;
+
+    private slots:
+    void addLinkConfig();
+    void removeLinkConfig();
+    void updateManipulatorModel();
 
 };
 }
--- src/views/user_config_view.cpp
+++ src/views/user_config_view.cpp
@@ -1,20 +1,93 @@
 #include "c_space_manipulator/views/user_config_view.hpp"
 
-cspace_manipulator::UserConfigView(QWidget* parent) : QWidget(parent)
+namespace cspace_manipulator
 {
-    // By default 2 Links are there
-    // Link 1
-    LinkConfigView default_link1 = new LinkConfigView(1, this);
+UserConfigView::UserConfigView(QWidget* parent) : QWidget(parent)
+{
+    // Scroll Area
+    QScrollArea* scroll_area = new QScrollArea(this);
+    scroll_area->setWidgetResizable(true);
 
-    // Link 2
-    LinkConfigView default_link2 = new LinkConfigView(2, this);
+    // Scroll Content
+    QWidget* scroll_content = new QWidget(this);
+    QVBoxLayout* scroll_layout = new QVBoxLayout(scroll_content);
+
+    // Link Content
+    links_layout_ = new QVBoxLayout();
+    links_layout_ ->setAlignment(Qt::AlignTop);
+    scroll_layout->addLayout(links_layout_);
 
     // Add/Remove Latest Link Button
-    
+    QPushButton* add_button = new QPushButton(tr("add"),this);
+    QPushButton* remove_button = new QPushButton(tr("remove"),this);
+    connect(add_button, &QPushButton::clicked, this, &UserConfigView::addLinkConfig);
+    connect(remove_button, &QPushButton::clicked, this, &UserConfigView::removeLinkConfig);
 
-    // Scroll Bar
+    // Button Layout
+    QHBoxLayout* button_layout = new QHBoxLayout();
+    button_layout->addWidget(add_button);
+    button_layout->addWidget(remove_button);
+    scroll_layout->addLayout(button_layout);
 
-    // Update Button
+    scroll_content->setLayout(scroll_layout);
+    scroll_area->setWidget(scroll_content);
 
-    // Layout
+    // User Config Layout
+    QVBoxLayout* user_config_layout = new QVBoxLayout(this);
+    user_config_layout->addWidget(scroll_area);
+    setLayout(user_config_layout);
+
+    // By default add 2 Links
+    addLinkConfig();
+    addLinkConfig();
+
 }
+
+void UserConfigView::addLinkConfig()
+{
+    // Invalid if max number of link is reached
+    if(link_config_list_.size() >= max_link_num_)
+    {
+        // Warn Message Pop Up
+        QMessageBox::warning(
+            this,
+            tr("Maximum Links Reached"),
+            tr("Cannot exceed maximum link number %1").arg(max_link_num_)
+        );
+        return;
+    }
+
+    // Create new link
+    int new_index = link_config_list_.size() + 1;
+    LinkConfigView* new_link = new LinkConfigView(new_index, this);
+    link_config_list_.push_back(new_link);
+
+    // Add to layout
+    links_layout_->addWidget(new_link);
+
+}
+
+void UserConfigView::removeLinkConfig()
+{
+    if(link_config_list_.size() == 0)
+    {
+        // Warn Message Pop Up
+        QMessageBox::warning(
+            this,
+            tr("No Links"),
+            tr("There are no Links to remove !")
+        );
+        return;
+    }
+
+    // Remove link from layout
+    LinkConfigView* last_link = link_config_list_.back();
+    links_layout_->removeWidget(last_link);
+
+    // Remove link from memory
+    last_link->deleteLater();
+    link_config_list_.pop_back();
+
+}
+
+} // end of namespace
